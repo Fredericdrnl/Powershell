a551fa89d528d965aa0ea00e42eb6bed68cbe915
Frédéric DOURNEL <fredericdournel@gmail.com>
2023-09-14 09:31:05 +0200

    add comment

--- tri.ps1
+++ tri.ps1
@@ -9,18 +9,31 @@ Param
     )
     
 Write-Host $folderPath
+# Take files on the folder 
 $filesDL = Get-ChildItem -Path $folderPath -Force
+
+# For each file
 foreach ($file in $filesDL){
+
+    # Get extension of the file
     $extension =[System.IO.Path]::GetExtension($file)
+
+    # If the fil
     if (!($extension -like $file)){
+        
+        # If the file isn't a folder
         if (!($extension -like "")){
             Write-Host "$($extension)"
-            Write-Host "$($folderPath)$extension"
-            if (!(Test-Path "$($folderPath)$($extension)")){
-                New-Item "$($folderPath)$extension" -itemType Directory
+            Write-Host "$($folderPath)\$extension"
+            
+            #IF the folder exist
+            if (!(Test-Path "$($folderPath)\$($extension)")){
+                New-Item "$($folderPath)\$extension" -itemType Directory
             }
-            Copy-Item "$($folderPath)$($file)" -Destination "$($folderPath)$extension" 
-            Remove-Item -Path "$($folderPath)$($file)"
+
+            # Copy and paste file
+            Copy-Item "$($folderPath)\$($file)" -Destination "$($folderPath)\$extension" 
+            Remove-Item -Path "$($folderPath)\$($file)"
         }
     }
 }
