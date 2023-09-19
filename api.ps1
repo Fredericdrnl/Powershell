83c1d0bf5353536fb30d427082b6e4d755e8824e
Frédéric DOURNEL <fredericdournel@gmail.com>
2023-09-19 13:18:10 +0200

    update code

--- api.ps1
+++ api.ps1
@@ -1,33 +1,23 @@
 Param
     (
         [Parameter(Mandatory=$true)]
-        [string] $codePostale
-    )
-    (
-        [string] $codePostale2
-    )
-    (
-        [string] $codePostale3
-    )
-    (
-        [string] $codePostale4
-    )
-    (
-        [string] $codePostale5
-    )
-    (
+        [string] $codePostale,
+        [string] $codePostale2,
+        [string] $codePostale3,
+        [string] $codePostale4,
+        [string] $codePostale5,
         [string] $codePostale6
     )
 $code = $codePostale, $codePostale2, $codePostale3, $codePostale4, $codePostale5, $codePostale6
 $index = 0
-while (!($code[0] -ilike $null) -and ($index -lt $code.Count - 1)) {
-    Write-Host $index
-    Write-Host $code.Count
-    $departementCode = $codePostale.Substring(0,2) 
+while (!($code[$index] -like $null) -and ($index -lt $code.Count)) {
+    Write-Host $code[$index]
+    $departementCode = $code[$index].Substring(0,2) 
     $urlCommunes = "https://geo.api.gouv.fr/departements/$($departementCode)/communes"
     $listCommunes = Invoke-RestMethod -Uri $urlCommunes -Method 'Get'
     foreach( $commune in $listCommunes ){
         if ( $codePostale -eq $commune.code ){
+            Write-Host $codePostale
             $nomCommune = $commune.nom
             $populationCommune = $commune.population
             $codePostaleCommune = $commune.code 
