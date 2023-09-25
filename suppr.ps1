e885e786165276d55ab30e1e6c78eb6461109c22
Frédéric DOURNEL <fredericdournel@gmail.com>
2023-09-25 16:43:23 +0200

    R

--- suppr.ps1
+++ suppr.ps1
@@ -20,26 +20,23 @@ Write-Host $folderPath
 $filesDL = Get-ChildItem -Path $folderPath -Force
 
 if (!(Test-Path -Path ".\logs.txt")) {
-    New-Item -Path . -Name "logs.txt" -ItemType "file" -Value "Time, Time exc, file`n"
+    New-Item -Path . -Name "logs.txt" -ItemType "file" -Value "Time, Time exc, file, Time in sec, Time exc in sec`n"
 }
 
 # For each file
 foreach ($file in $filesDL){
-    Write-Host $filesDL
-
+    
     #Take the last exc time of the file
     $timeExc = ($file.LastAccessTime).ToFileTime()
-    Write-Host $timeExc
 
     $limite = (Get-Date).addDays($days).ToFileTime()
-    Write-Host $limite
 
     #If currently time - the last time of exec > time
     if ($timeExc - $limite -lt 0){
         #Remove file
-        # Remove-Item -Path "$($folderPath)\$($file)"
+        Remove-Item -Path "$($folderPath)\$($file)"
         Write-Host "$($file) removed"
-        Add-Content -Path ".\logs.txt" -Value "$(Get-Date), $($timeExc), $($file)"
+        Add-Content -Path ".\logs.txt" -Value "$(Get-Date), $($file.LastAccessTime), $($file), $($limite), $($timeExc)"
     }
     Write-Host "-----------------"
 }
