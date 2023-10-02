a5054eaa92cb0dfb0a49186adc07ac9b34f3d4cb
Frédéric DOURNEL <fredericdournel@gmail.com>
2023-10-02 10:01:42 +0200

    change

--- Set-UserAD/Set-UserAD.ps1
+++ Set-UserAD/Set-UserAD.ps1
@@ -21,12 +21,15 @@ function Set-UserAD {
     # }
 
     # Connexion à l'AD
-    try {
-        Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
-    } catch {
-        Write-Verbose "Compte invalide"
-        throw "Compte invalide"
-    }
+    # try {
+    #     Connect-MgGraph -TenantId "2362ee81-c9fc-4f6b-a794-6e91518e5c13" -ClientId "83efbd84-808a-4478-b30c-16df66cd410c" -ClientSecret "-~B8Q~KzTrZ7YY9iGu~Ygtx538wEFbvAQJyHHcJ9"
+    #     Get-MgUser
+    # } catch {
+    #     Write-Verbose "Compte invalide"
+    #     throw "Compte invalide"
+    # }
+    Connect-MgGraph -TenantId "2362ee81-c9fc-4f6b-a794-6e91518e5c13" -ClientId "83efbd84-808a-4478-b30c-16df66cd410c"
+    Get-MgUser
 }
 # Set-UserAD -jsonPath $PSBoundParameters['jsonPath']  -accountPath $PSBoundParameters['accountPath']
 Set-UserAD -jsonPath ".\user.json"  -accountPath ".\account.json"
