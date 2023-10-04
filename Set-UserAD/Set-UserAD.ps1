699bce68052fd3e6dbe37314bbd4fe9df499dd4d
Frédéric DOURNEL <fredericdournel@gmail.com>
2023-10-04 16:22:54 +0200

    add connect

--- Set-UserAD/Set-UserAD.ps1
+++ Set-UserAD/Set-UserAD.ps1
@@ -9,27 +9,25 @@ function Set-UserAD {
         [string] $accountPath
     )
 
-    $Users = Get-Content -Path $($PSBoundParameters['jsonPath']) | ConvertFrom-Json
-    $Users
     # Récupération des comptes dans le json
-    # try {
-    #     $Users = Get-Content -Path $($PSBoundParameters['jsonPath']) | ConvertFrom-Json
-    #     Write-Verbose $Users
-    # } catch {
-    #     Write-Error "Fichier json invalide"
-    #     throw "Fichier json invalide"
-    # }
+    try {
+        $Users = Get-Content -Path "$(Get-Location)\$($PSBoundParameters['jsonPath'])" | ConvertFrom-Json
+        $Users
+    } catch {
+        Write-Error "Fichier json invalide"
+        throw "Fichier json invalide"
+    }
 
     # Connexion à l'AD
-    # try {
-    #     Connect-MgGraph -TenantId "2362ee81-c9fc-4f6b-a794-6e91518e5c13" -ClientId "83efbd84-808a-4478-b30c-16df66cd410c" -ClientSecret "-~B8Q~KzTrZ7YY9iGu~Ygtx538wEFbvAQJyHHcJ9"
-    #     Get-MgUser
-    # } catch {
-    #     Write-Verbose "Compte invalide"
-    #     throw "Compte invalide"
-    # }
-    Connect-MgGraph -TenantId "2362ee81-c9fc-4f6b-a794-6e91518e5c13" -ClientId "83efbd84-808a-4478-b30c-16df66cd410c"
+    try {
+        $CliendId = "83efbd84-808a-4478-b30c-16df66cd410c"
+        $ClientSecret = ConvertTo-SecureString -String "-~B8Q~KzTrZ7YY9iGu~Ygtx538wEFbvAQJyHHcJ9" -AsPlainText -Force
+        $SecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $CliendId, $ClientSecret
+        $TenantId = "2362ee81-c9fc-4f6b-a794-6e91518e5c13"
+        Connect-MgGraph -CLientSecretCredential $SecretCredential -TenantId $TenantId
+    } catch {
+        Write-Verbose "Compte invalide"
+        throw "Compte invalide"
+    }
     Get-MgUser
 }
-# Set-UserAD -jsonPath $PSBoundParameters['jsonPath']  -accountPath $PSBoundParameters['accountPath']
-Set-UserAD -jsonPath ".\user.json"  -accountPath ".\account.json"
